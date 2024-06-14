77ebc1041647c9f2428236482f5ef6b2f395b8e6
dannykim1837 <eoghks1837@gmail.com>
2024-06-14 11:14:43 -0600

    restart button

--- game.js
+++ game.js
@@ -10,62 +10,77 @@ const wordDisplay = document.querySelector(".word-display")
 const scoreDisplay = document.querySelector(".score")
 const timeDisplay = document.querySelector(".time")
 const button = document.querySelector(".button")
-
+const restartButton = document.querySelector(".restart-button");
 
 init();
+
 function init() {
-    getWords()
-    wordInput.addEventListener("input", checkMatch)
+    getWords();
+    wordInput.addEventListener("input", checkMatch);
+    button.addEventListener("click", run);
+    restartButton.addEventListener("click", restartGame);
 }
 
 function run() {
-    if (isPlaying){
+    if (isPlaying) {
         return;
     }
     isPlaying = true;
     time = Game_Time;
-    wordInput.focus()
-    scoreDisplay.innerText = 0;
+    wordInput.focus();
+    score = 0;
+    scoreDisplay.innerText = score;
     timeInterval = setInterval(countDown, 1000);
-    checkInterval = setInterval(checkInterval, 50)
-    buttonChange("playing")
+    checkInterval = setInterval(checkStatus, 50);
+    buttonChange("Playing");
+}
+
+function restartGame() {
+    isPlaying = false;
+    restartButton.style.display = "none";
+    run();
 }
 
 function checkStatus() {
-    if(!isPlaying && time === 0)
-        buttonChange("Game Start")
-        clearInterval(checkInterval)
-}
-function getWords(){
-    words = ["Hello", "Apple", "Banana"];
-    buttonChange("Game Start")
-}
+    if (!isPlaying && time === 0) {
+        buttonChange("Game Over");
+        clearInterval(checkInterval);
+        button.disabled = true; 
+        restartButton.style.display = "inline-block";
 
-
-
-
-function checkMatch (){
-    if (wordInput.value === wordDisplay.innerText){
-        wordInput.value = "";
-        score++;
-        scoreDisplay.innerText = score;
-        time = Game_Time;
-        const randomIndex = Math.floor(Math.random() * words.length);
-        wordDisplay.innerText = words[randomIndex]
     }
 }
 
+function getWords() {
+    words = ["Hello", "Apple", "Banana"];
+    buttonChange("Game Start");
+}
 
+function checkMatch() {
+    if (wordInput.value === wordDisplay.innerText) {
+        wordInput.value = "";
+        score++;
+        scoreDisplay.innerText = score;
+        const randomIndex = Math.floor(Math.random() * words.length);
+        wordDisplay.innerText = words[randomIndex];
+    }
+}
 
 function countDown() {
     time > 0 ? time-- : isPlaying = false;
-    if(!isPlaying){
-        clearInterval(timeInterval)
+    if (!isPlaying) {
+        clearInterval(timeInterval);
     }
     timeDisplay.innerText = time;
 }
 
 function buttonChange(text) {
     button.innerText = text;
-    text === "Game Start" ?  button.classList.remove('loading') : button.classList.add('loading')
+    if (text === "Game Start") {
+        button.classList.remove('loading');
+        button.disabled = false;
+    } else {
+        button.classList.add('loading');
+    }
 }
+
