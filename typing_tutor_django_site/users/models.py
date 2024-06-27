1480d257bb5c65396284a7bbfe73ccdb98720430
Talon Hintze <114630220+hintzeto@users.noreply.github.com>
2024-06-26 18:13:24 -0600

    working on updating database stuff

--- typing_tutor_django_site/users/models.py
+++ typing_tutor_django_site/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Rank(models.Model):
     rankname = models.CharField(max_length=20)
     points = models.IntegerField()
-    icon = models.CharField(max_length=20)
+    icon = models.CharField(max_length=300)
 
     def __str__(self):
         return self.rankname
@@ -14,6 +14,7 @@ class User(models.Model):
     fname = models.CharField(max_length=20)
     lname = models.CharField(max_length=20)
     username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20, null=True)
     points = models.IntegerField()
     rank = models.ForeignKey(Rank, on_delete=models.CASCADE, null=True, blank=True)
 
--- typing_tutor_django_site/users/views.py
+++ typing_tutor_django_site/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import User
 
 # Create your views here.
+
+def users(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
