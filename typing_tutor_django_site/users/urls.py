b58483b096fc478964970738ca3a94c49b1b7df1
Talon Hintze <114630220+hintzeto@users.noreply.github.com>
2024-07-01 11:06:56 -0600

    Creating master template for users

--- typing_tutor_django_site/users/urls.py
+++ typing_tutor_django_site/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('users/', views.users, name='users'),
-    path('members/details/<int:id>', views.details, name='details')
+    path('users/details/<int:id>', views.details, name='details')
 ]
