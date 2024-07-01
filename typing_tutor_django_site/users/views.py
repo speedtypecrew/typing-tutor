fbd4d987e366fddc176ac7d0a8a465c15c38c902
Talon Hintze <114630220+hintzeto@users.noreply.github.com>
2024-07-01 10:52:58 -0600

    Testing more database stuff

--- typing_tutor_django_site/users/views.py
+++ typing_tutor_django_site/users/views.py
@@ -5,5 +5,17 @@ from .models import User
 # Create your views here.
 
 def users(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  myusers = User.objects.all().values()
+  template = loader.get_template('all_users.html')
+  context = {
+    'myusers': myusers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  myuser = User.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'myuser': myuser,
+  }
+  return HttpResponse(template.render(context, request))
