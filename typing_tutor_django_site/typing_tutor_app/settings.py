315c2ce0f7994908d9dbd110f7a33d764b6213ae
dannykim1837 <eoghks1837@gmail.com>
2024-07-08 10:45:29 -0600

    css

--- typing_tutor_django_site/typing_tutor_app/settings.py
+++ typing_tutor_django_site/typing_tutor_app/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
